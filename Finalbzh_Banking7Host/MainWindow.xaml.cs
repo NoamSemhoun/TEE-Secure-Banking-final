e4f5ed70c580a547f9f42e457834dcb62eb88274
NoamSemhoun <noam@semhoun.eu>
2022-08-30 18:33:57 +0300

    test

--- Finalbzh_Banking7Host/MainWindow.xaml.cs
+++ Finalbzh_Banking7Host/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System.Windows.Data;//
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
